dcd28ddb97d2b29f06776d6d2d83d5968273b2fa
Jeffrey Canty <jeffrey.canty@casper.com>
2018-04-04 16:13:25 -0400

    Format files and update package.json

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-import SketchField from './SketchField'
-import Tools from './tools'
+import SketchField from './SketchField';
+import Tools from './tools';
 
-export {SketchField}
-export {Tools}
+export { SketchField };
+export { Tools };
 
 export default {
-    SketchField,
-    Tools
+  SketchField,
+  Tools
 };
